0fb21c0a7795944a1ddb249f4630fbe1b8d3ee66
cyldztkn <cagkanyildiztekin14@gmail.com>
2024-10-07 23:39:56 +0300

    "response json cors problemi çözüldü"

--- index.js
+++ index.js
@@ -3,26 +3,9 @@ const http = require("http");
 // Sunucu oluştur
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <button id="buton">Fetch Data</button>
-
-    <script>
-      let buttonEl = document.getElementById("buton");
-      buttonEl.addEventListener('click', ()=> {
-        fetch('http://localhost:3000/').then(res => console.log(res))
-      })
-    </script>
-  </body>
-</html>
-`);
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // CORS başlığını ekliyoruz
+  res.end(JSON.stringify({ message: "Merhaba Ninja Expresso!" }));
 });
 
 // Sunucuyu belirli bir portta dinlet
