ddb308c711b498a268b3f421c5f8c458c396e3e8
cyldztkn <cagkanyildiztekin14@gmail.com>
2024-10-16 01:52:43 +0300

    basit routing yapısı

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Temel bir GET isteği (route)
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// GET isteği: Veri almak için kullanılır
+app.get('/getExample', (req, res) => {
+    res.send('GET isteği ile veri alındı');
 });
 
-// Sunucuyu belirtilen port üzerinden başlatıyoruz
+// POST isteği: Veri oluşturmak veya sunucuya veri göndermek için kullanılır
+app.post('/postExample', (req, res) => {
+    res.send('POST isteği ile veri gönderildi');
+});
+
+// PUT isteği: Mevcut bir veriyi güncellemek için kullanılır
+app.put('/putExample', (req, res) => {
+    res.send('PUT isteği ile veri güncellendi');
+});
+
+// DELETE isteği: Mevcut bir veriyi silmek için kullanılır
+app.delete('/deleteExample', (req, res) => {
+    res.send('DELETE isteği ile veri silindi');
+});
+
 app.listen(PORT, () => {
-  console.log(`Sunucu "http://localhost:${PORT}" portunda çalışıyor...`);
+    console.log(`Sunucu ${PORT} portunda çalışıyor...`);
 });
