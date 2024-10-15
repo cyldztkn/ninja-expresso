5a6e19a250259b5c0f242850baec221e302a6271
cyldztkn <cagkanyildiztekin14@gmail.com>
2024-10-15 03:40:23 +0300

    "Express"

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const os = require('os');
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-// İşletim sistemi bilgileri
-console.log(`Platform: ${os.platform()}`);
-console.log(`Mimari: ${os.arch()}`);
-console.log(`CPU'lar: ${JSON.stringify(os.cpus(), null, 2)}`);
-console.log(`Toplam Bellek: ${os.totalmem()} bayt`);
-console.log(`Boş Bellek: ${os.freemem()} bayt`);
-console.log(`Sistem Uptime: ${os.uptime()} saniye`);
-console.log(`Ev Dizini: ${os.homedir()}`);
-console.log(`Ağ Arayüzleri: ${JSON.stringify(os.networkInterfaces(), null, 2)}`);
+app.get('/', (req, res) => {
+  res.send('Merhaba Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor.`);
+});
